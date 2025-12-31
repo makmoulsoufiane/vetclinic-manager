749c7e0784f8fcf80839cab43132ad9e1b59df7f
makmoul soufiane <makmoulsoufiane2@gmail.com>
2025-12-31 22:01:45 +0100

    instal react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Routes, Route } from "react-router-dom";
+// import Home from "./pages/Home";
 
 function App() {
   return (
-    <div>App1</div>
+    <div>app</div>
   )
 }
 
